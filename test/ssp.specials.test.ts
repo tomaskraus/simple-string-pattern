50ced7ce2070398e8cced5b499ed9552ff0d9ac3
Tomas Kraus <tomas.kraus.dev@gmail.com>
2024-07-10 15:30:00 +0200

    refactor(test): simplify data struct

--- test/ssp.specials.test.ts
+++ test/ssp.specials.test.ts
@@ -3,29 +3,21 @@ import u from './test-utils';
 
 describe('Pattern: specials', () => {
   test('value() invariants.', () => {
-    const patterns = [
-      ['""', ''],
-      ['Hello', 'Hello'],
-      ["That's a 😀! きáéÜΔ", "That's a 😀! きáéÜΔ"],
-      ['That\'s "inside" it.', 'That\'s "inside" it.'],
-      [
-        '" leading and trailing spaces matter.  "',
-        ' leading and trailing spaces matter.  ',
-      ],
-      ['"Double quote at the start.', '"Double quote at the start.'],
-      ['.', '.'],
-      ['..', '..'],
-      ['. .', '. .'],
-      ['Two line\\n text', 'Two line\n text'],
-      ['\\" (\\\', \\") \\"', '" (\', ") "'],
-      ['\\\\ escaped backslash: \\\\.', '\\ escaped backslash: \\.'],
-      [
-        'A literal form of newline escape sequence: \\\\n.',
-        'A literal form of newline escape sequence: \\n.',
-      ],
-    ];
-
-    patterns.forEach(([pattStr, _]) => {
+    [
+      '""',
+      'Hello',
+      "That's a 😀! きáéÜΔ",
+      'That\'s "inside" it.',
+      '" leading and trailing spaces matter.  "',
+      '"Double quote at the start.',
+      '.',
+      '..',
+      '. .',
+      'Two line\\n text',
+      '\\" (\\\', \\") \\"',
+      '\\\\ escaped backslash: \\\\.',
+      'A literal form of newline escape sequence: \\\\n.',
+    ].forEach(pattStr => {
       [u.id, u.toStartPatt, u.toEndPatt, u.toMiddlePatt].forEach(pattModfn => {
         const patt = new SSP(pattModfn(pattStr));
         // console.log('pattV:', patt.value());
